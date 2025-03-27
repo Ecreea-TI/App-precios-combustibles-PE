3b6065b6eaec778f1fbc3d26adced631deb28685
Ecreea-Mk <mkecreea@gmail.com>
2025-03-27 08:28:27 -0500

    added package.json

--- api/api.js
+++ api/api.js
@@ -1,5 +1,8 @@
-const { handler } = require('../netlify/functions/api');
+const { handler } = require('@netlify/functions');
+const { app } = require('../netlify/functions/api');
+const { Mangum } = require('mangum');
 
 exports.handler = async (event, context) => {
-  return await handler(event, context);
+  const mangum = new Mangum(app);
+  return await mangum(event, context);
 };
